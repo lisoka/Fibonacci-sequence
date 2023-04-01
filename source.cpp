7c264a464586d747ff902eed89f55a356e7418f3
Angelina Stiukhina <a.stiukhina@gmail.com>
2023-04-01 18:10:03 +0300

    iter fib sequence (0, 1, ...), n first numbers

--- source.cpp
+++ source.cpp
@@ -5,17 +5,28 @@
 using namespace std;
 
 
-ull fib(ull n) {
-        if (n < 1) return ULLONG_MAX;
-        if (n == 1) return 0;
-        if (n == 2) return 1;
-        return fib(n - 1) + fib(n - 2);
+void fib(ull n) {
+        if (n < 1) {
+            cout << ULLONG_MAX << endl;
+            return;
+        }
+        ull fib_1 = 0;
+        ull fib_2 = 1;
+        if (n == 1) {
+            cout << fib_1 << endl;
+            return;
+        }
+        cout << fib_1 << endl << fib_2 << endl;
+        for (int i = 2; i < n; ++i) {
+            fib_2 = fib_2 + fib_1;
+            fib_1 = fib_2 - fib_1;
+            cout << fib_2 << endl;
+	}
 }
 
 
 int main() {
         ull n;
         cin >> n;
-        cout << fib(n) << endl;
+        fib(n);
 }
-
